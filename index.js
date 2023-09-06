78b159b2d8d3a1bb84570164fed8e0d921bce5a9
Unqown Exe <76444282+unqownexe@users.noreply.github.com>
2023-09-06 11:37:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+import express from "express"
 const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(5001)
+app.listen(22)
